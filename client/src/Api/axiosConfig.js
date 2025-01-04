31e1f7cc43d721d592971dc6a69c2c040069dfed
Your Name <you@example.com>
2025-01-04 04:53:17 +0100

    client

--- client/src/Api/axiosConfig.js
+++ client/src/Api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadi-forum-groupa-backend-5.onrender.com",
+  baseURL: "http://localhost:5500/api",
+  // baseURL: "https://evangadi-forum-groupa-backend-5.onrender.com",
 });
 
 export default axiosBase;
